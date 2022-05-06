a478ed8059428e13b3a344950c808e73d1584b17
unknown <rodrigo.kloeckner@gmail.com>
2022-05-06 15:52:47 -0300

    add port and variavble

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import { routes } from "./routes";
-import cors from "cors";
+import express from 'express';
+import { routes } from './routes';
+import cors from 'cors';
 
 const app = express();
 
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log("HTTP server running");
+app.listen(process.env.PORT || 3333, () => {
+  console.log('HTTP server running');
 });
